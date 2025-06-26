e2fe967a63f8a581c8099e332485091f82838771
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-06-26 12:18:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 import CreateProject from "./components/CreateProject";
-import ProjectList from "./components/ProjectList";
 import ProjectSidebar from "./components/ProjectSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectList from "./components/ProjectList";
 function App() {
+  const [] = useState();
   return (
     <main className="h-screen my-8  flex gap-8 ">
       <ProjectSidebar />
-      <CreateProject />
+      {/* <CreateProject /> */}
+      <NoProjectSelected />
     </main>
   );
 }
